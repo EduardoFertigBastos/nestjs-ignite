a2dcf62d1c7baf885e4770a8feb6830a72724b64
Eduardo Fertig Bastos <dudufbastos1@gmail.com>
2024-01-24 00:48:50 -0300

    prisma questions mapper to prisma

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,23 +1,40 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Question } from '@/domain/forum/enterprise/entities/question';
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
-import { Question as PrismaQuestion } from '@prisma/client';
+import { Question as PrismaQuestion, Prisma } from '@prisma/client';
 
 export class PrismaQuestionMapper {
   static toDomain(raw: PrismaQuestion): Question {
     return Question.create(
       {
-        title: raw.title,
-        content: raw.content,
         authorId: new UniqueEntityID(raw.authorId),
         bestAnswerId: raw.bestAnswerId
           ? new UniqueEntityID(raw.bestAnswerId)
           : undefined,
+
         slug: Slug.create(raw.slug),
+
+        title: raw.title,
+        content: raw.content,
         createdAt: raw.createdAt,
         updatedAt: raw.updatedAt,
       },
       new UniqueEntityID(raw.id),
     );
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+
+      slug: question.slug.value,
+
+      title: question.title,
+      content: question.content,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    };
   }
 }
