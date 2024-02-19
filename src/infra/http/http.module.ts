e65af686b309650b702d1a2a3afbee5225231a7e
Eduardo Fertig Bastos <dudufbastos1@gmail.com>
2024-02-19 20:48:50 -0300

    Update http.module.ts

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -9,6 +9,10 @@ import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-case
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student';
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student';
 import { CryptographyModule } from '../cryptography/cryptography.module';
+import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller';
+import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug';
+import { EditQuestionController } from './controllers/edit-question.controller';
+import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -17,12 +21,16 @@ import { CryptographyModule } from '../cryptography/cryptography.module';
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
+    GetQuestionBySlugController,
+    EditQuestionController,
   ],
   providers: [
     CreateQuestionUseCase,
     FetchRecentQuestionsUseCase,
     AuthenticateStudentUseCase,
     RegisterStudentUseCase,
+    GetQuestionBySlugUseCase,
+    EditQuestionUseCase,
   ],
 })
 export class HttpModule {}
